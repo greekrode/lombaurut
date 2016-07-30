fb91e8d2a04d99894a7b83a0a51b34d88115dbd8
Roderick Halim <roderickhalim@gmail.com>
2016-07-30 14:22:01 +0700

    ERROR CHECKED

--- src/lombaurut/Lombaurut.java
+++ src/lombaurut/Lombaurut.java
@@ -21,73 +21,18 @@ public class Lombaurut {
         // BEGIN - YOUR CODE HERE
         // =====================================================================
         //
-          //radix sorting used
-//        int i, m = x[0], exp = 1, n = x.length;
-//        int[] b = new int[10]; //bucket
-//        for (i = 1; i < n; i++)
-//            if (x[i] > m){
-//                m = x[i]; }		   //getmax
-//				
-//        while (m / exp > 0){                   //max didiv pangkat 10 diatas 0, bucket dibuat lagi
-//            int[] bucket = new int[10];        //initialisasi bucket
-//            for (i = 0; i < n; i++){
-//                bucket[(x[i] / exp) % 10]++;   //dimasukkan ke bucket 
-//			}
-//            for (i = 1; i < 10; i++){       //bucket dituang ke bucket paling atas
-//                bucket[i] += bucket[i - 1];
-//			}
-//            for (i = n - 1; i >= 0; i--){   //sorting ascending
-//                b[--bucket[(x[i] / exp) % 10]] = x[i];
-//			}
-//            for (i = 0; i < n; i++){
-//                x[i] = b[i];
-//                }
-//            exp *= 10;        
-//
-//        }
-	
- //         InsertionSort
+          // InsertionSort
         int i,j,k,temp,l;
-        Boolean asdf = true;
+      Boolean asdf = true;
         temp = 0;
-        l = x.length;
+       l = x.length;
             for(j = 1; j <x.length-1;j++){
                 k = x[j];
-                for(i = j-1;i>=0&&(x[i]<k); i --){
-                    x[i+1] = x[i];
+               for(i = j-1;i>=0&&(x[i]<k); i --){
+                   x[i+1] = x[i];
             }
-                x[i+1] = k;
+               x[i+1] = k;
             }
-            //flipping
-         Xutil.writeTxtFileFromArray(x, "sorted.dat");      
-         int[] y = Xutil.readTxtFileToArray("sorted.dat");
-         
-            for(j = (x.length)-1;j > x.length/2;j--){ // 99 999 to 50 000 
-                
-                y[j] = temp; // value of 99999 to hold
-                y[j] = y[(l-j)-1]; // 100000-99999 (?) l-j-1 = 100000 - 99999 - 1
-                y[(l-j)-1] = temp; // 100000 - 99999(looped)  
-                }
-             Xutil.writeTxtFileFromArray(y, "sorted.dat");
-            // 0 to 49 999
-
-//shift f6 = result from descending
-            //SelectionSort
-//            int i, j, minIndex, tmp;
-//            int n = x.length;
-//                for (i = 0; i < n - 1; i++) {
-//                   minIndex = i; 
-//                   for (j = i + 1; j < n; j++){
-//                     if (x[j] < x[minIndex])
-//                     minIndex = j;
-//                     if (minIndex != i) {
-//                        tmp = x[i];
-//                        x[i] = x[minIndex];
-//                        x[minIndex] = tmp;
-//                     }
-//                }
-//            }
-//        
         // =====================================================================
         // END
         
@@ -95,7 +40,7 @@ public class Lombaurut {
         long end = Xutil.getTime();
 
         System.out.println("Total execution time (nano sec): " + Xutil.executionTime(start, end));       
-       
+        Xutil.writeTxtFileFromArray(x, "sorted.dat");
     }
 
 }
